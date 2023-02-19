60ef672511b0442fb2d10948a725269fec2f073e
Thiago Vinícius <80261904+ubyss@users.noreply.github.com>
2023-02-19 19:52:06 -0300

    Add project files.

---
+++ Thiago_Vinicius_TP1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Thiago_Vinicius_TP1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
